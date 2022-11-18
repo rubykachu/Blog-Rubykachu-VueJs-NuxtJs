6a0663860c23e96f9349ecd2f34307e73b0ecbac
Tang Quoc Minh <vhquocminhit@gmail.com>
2022-11-18 14:14:04 +0700

    Change icon facebook

--- components/Associate.vue
+++ components/Associate.vue
@@ -1,1 +1,7 @@
-<template></template>
+<template>
+  <div>
+    <a href="https://www.facebook.com/tangquocminh" target="blank" rel="nofollow"
+      ><v-icon color="white" class="px-1">mdi-thumb-up</v-icon></a
+    >
+  </div>
+</template>
